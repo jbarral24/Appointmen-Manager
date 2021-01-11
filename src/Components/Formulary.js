2a6ca06d27603c18adc6cd28289fabefd4f17114
jbarral24 <joseluisbarraljr@gmail.com>
2021-01-11 19:31:57 -0300

    Added Hour input

--- src/Components/Formulary.js
+++ src/Components/Formulary.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Formulary = () => {
+
+const [Appointment, actualizeApointment] = useState({
+
+pet: '',
+Owner: '',
+
+
+})
+
   return (
     <React.Fragment>
       <h2>Create Appointment</h2>
@@ -14,16 +23,18 @@ const Formulary = () => {
           placeholder="Pet name"
         />
 
-        <label> Pet Owner</label>
+        <label> Owner</label>
         <input
           type="text"
-          name="Pet Owner"
+          name="Owner"
           className="u-full-width"
           placeholder="Pet owner name"
         />
 
         <label> Date</label>
         <input type="date" name="date" className="u-full-width" />
+        <label> pick up hour</label>
+        <input type="time" name="hour" className="u-full-width" />
       </form>
     </React.Fragment>
   );
